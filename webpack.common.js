26fa72106f45d8ea1c00de8ebd8424fdf2eda861
LeeMir <manofgodmj@naver.com>
2021-09-22 17:53:54 +0900

    [webpack] get and set css, sass loader

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = { 
@@ -13,15 +14,23 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: "Webpack-Boilerplate",
       template: path.resolve(__dirname, "src", "app.html")
-    })
+    }),
+    new MiniCssExtractPlugin({ filename: "style.css" })
   ],
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: "babel-loader"
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       }
-    }, ],
+    ],
   }
 };
