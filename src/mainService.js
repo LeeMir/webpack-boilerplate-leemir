dec09504143b660c88e1990205da3e7ed443f072
LeeMir <manofgodmj@gmail.com>
2021-09-24 00:26:40 +0900

    fix tab size 4 => 2

--- src/mainService.js
+++ src/mainService.js
@@ -1,18 +1,18 @@
 import "./css/test.scss";
 
 export default class MainService  {
-    constructor({targetEl}) {
-        this.targetEl = targetEl;
-        this.dataList = "";
-    }
+  constructor({targetEl}) {
+    this.targetEl = targetEl;
+    this.dataList = "";
+  }
 
-    init(dataList) {
-        this.dataList = dataList.flat(2).join('');
-        return this.render(this.dataList);
-    }
+  init(dataList) {
+    this.dataList = dataList.flat(2).join('');
+    return this.render(this.dataList);
+  }
 
-    render(data) {
-        return `<span>${data}</span>`;
-    }
+  render(data) {
+    return `<span>${data}</span>`;
+  }
 
 }
