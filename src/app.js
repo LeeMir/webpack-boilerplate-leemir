e24ea752472fbd80d0e791b92c245c738eb5be61
LeeMir <manofgodmj@gmail.com>
2021-09-23 20:34:24 +0900

    test 환경

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 import MainService from './mainService.js';
+import Svg from './assets/images/test.svg';
 
 window.addEventListener("DOMContentLoaded", ()=> {
-    const targetEl = document.querySelector("#log");
+  const targetEl = document.querySelector("#log");
 
-    const service = new MainService({targetEl})
-    const datalist = [1,2,3,4,[5,6,[7]]];
-    const subHtml = service.init(datalist);
+  const service = new MainService({targetEl})
+  const datalist = [1,2,3,4,[5,6,[7]]];
+  const subHtml = service.init(datalist);
 
-    targetEl.innerHTML += `datalist is ${subHtml}`;
+
+  targetEl.innerHTML += `
+    <div class='sample-img'></div>
+    <h1>hello world</h1>
+    datalist is ${subHtml}
+    <p style='font-weight: bold'>bold</p>
+    <p style='font-weight: normal'>normal</p>
+    <img class='sample-svg' src=${Svg} />
+  `;
 })
